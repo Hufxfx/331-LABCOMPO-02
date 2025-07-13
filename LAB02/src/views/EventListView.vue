ff6a1febfd53371b72942f2431122682b76f1fac
Chamor Lahuna <Chamor_l@cmu.ac.th>
2025-07-13 16:05:08 +0700

    question 15.3 commit

---
+++ LAB02/src/views/EventListView.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import EventCard from '@/components/EventCard.vue';
+import type { Event } from '@/types'
+import {ref, onMounted} from 'vue'
+import axios from 'axios';
+
+const events = ref<Event[]>()
+
+onMounted(() => {
+  axios
+    .get('https://my-json-server.typicode.com/Hufxfx/331-db/events')
+    .then((response) => {
+      console.log(response.data)
+    })
+    .catch((error) => {
+      console.error('There wan an error', error)
+    })
+})
+</script>
+
+<template>
+  <h1>Events for Good</h1>
+    <!-- new element -->
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event"/>
+  </div>
+</template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
